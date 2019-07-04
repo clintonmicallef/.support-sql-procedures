25810446416ae8084d31279d98a2a0e778870333
lukaszhanusik <lukasz.hanusik@trustly.com>
2019-07-04 19:54:32 +0200

    Add update script and alias "updatesql"

--- install.sh
+++ install.sh
@@ -12,4 +12,12 @@ psqlrcLineEscaped="\\${psqlrcLine}"
 
 touch ${psqlrcFile}
 grep -qxF "${psqlrcLine}" ${psqlrcFile} || sed -i "" -e $'$ a\\\n'"${psqlrcLineEscaped}" ${psqlrcFile}
-echo $'Your psqlrc file:\n'"$(cat ${psqlrcFile})"
+echo $'Your psqlrc configuration:\n'"$(cat ${psqlrcFile})"
+
+gitRemote="https://0518c76da1808fe52255329f0d020aa39346b5d5@github.com/lukaszhanusik/support-sql-procedures.git"
+## alias updatesql="cd '/Users/lukaszhanusik/.support-sql-procedures' && git status && git pull https://0518c76da1808fe52255329f0d020aa39346b5d5@github.com/lukaszhanusik/support-sql-procedures.git"
+### aliasLine='alias updatesql="cd '\''/Users/lukaszhanusik/.support-sql-procedures'\'' && git status && git pull https://0518c76da1808fe52255329f0d020aa39346b5d5@github.com/lukaszhanusik/support-sql-procedures.git"'
+## alias updatesql="cd '/Users/lukaszhanusik/.support-sql-procedures' && ./update.sh"
+aliasLine='alias updatesql="cd '\''/Users/lukaszhanusik/.support-sql-procedures'\'' && ./update.sh"'
+
+echo ${aliasLine}
---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## cd $support-sql-procedures.git
+## $ git config --global alias.up '!git remote update -p; git merge --ff-only @{u}'
+## $ git up
+## fatal: no upstream configured for branch 'master'
+
+git status && git pull https://0518c76da1808fe52255329f0d020aa39346b5d5@github.com/lukaszhanusik/support-sql-procedures.git
