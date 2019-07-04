f88e6ced2d30af072d6b976f70ca78da8044d5d3
lukaszhanusik <lukasz.hanusik@trustly.com>
2019-07-05 01:42:40 +0200

    Fix help

--- help.sql
+++ help.sql
@@ -4,12 +4,8 @@
 
 \prompt 'Please enter a keyword or press enter to see full list', keyword
 
-
-SELECT aliasname, comment
-  FROM supportsqlaliases
+SELECT substring(FileName, '(.*)\.sql$') AS Alias,
+       Comment
+  FROM SupportSQL_Procedures
  WHERE TRUE
-   -- AND :'keyword' ILIKE '%' || REPLACE(REPLACE((supportsqlaliases.category)::text, '{','')::text,'}','') || '%'
-   -- Below my suggestions. First one which is commented look for exact word, therefore using ILIKE is probably better : )
-   -- AND ARRAY[:'keyword']::text[] && category
-   AND (CASE WHEN NULLIF(:'keyword','') IS NOT NULL THEN (array_to_string(category, ',') ILIKE '%' || :'keyword' || '%') ELSE TRUE END)
-   ;
+   AND (CASE WHEN NULLIF(:'keyword','') IS NOT NULL THEN format('%s %s', substring(FileName, '(.*)\.sql$'), Comment) ILIKE '%' || :'keyword' || '%' ELSE TRUE END);
