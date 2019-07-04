04fd7d587ab9e3c5d9f33d55b1f694d870391cf2
lukaszhanusik <lukasz.hanusik@trustly.com>
2019-07-04 19:08:10 +0200

    Create or update existing psqlrc file

--- install.sh
+++ install.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo hello world
+scriptDir=$(cd $(dirname "$0"); pwd)
+
+psqlrcFile="${HOME}/.psqlrc"
+psqlrcLine="\i '${scriptDir}/init.psql'"
+psqlrcLineEscaped="\\${psqlrcLine}"
+
+## echo "${scriptDir}/init.psql"
+## echo "${psqlrcLine}"
+## echo "${psqlrcLineEscaped}"
+## echo "${psqlrcFile}"
+
+touch ${psqlrcFile}
+grep -qxF "${psqlrcLine}" ${psqlrcFile} || sed -i "" -e $'$ a\\\n'"${psqlrcLineEscaped}" ${psqlrcFile}
+echo $'Your psqlrc file:\n'"$(cat ${psqlrcFile})"
