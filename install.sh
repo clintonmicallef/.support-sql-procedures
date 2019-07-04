87f55101dbd542748580f47a970a1ba2d3469e8a
lukaszhanusik <lukasz.hanusik@trustly.com>
2019-07-04 20:39:34 +0200

    Alias configuration

--- install.sh
+++ install.sh
@@ -5,19 +5,14 @@ psqlrcFile="${HOME}/.psqlrc"
 psqlrcLine="\i '${scriptDir}/init.psql'"
 psqlrcLineEscaped="\\${psqlrcLine}"
 
-## echo "${scriptDir}/init.psql"
-## echo "${psqlrcLine}"
-## echo "${psqlrcLineEscaped}"
-## echo "${psqlrcFile}"
-
 touch ${psqlrcFile}
 grep -qxF "${psqlrcLine}" ${psqlrcFile} || sed -i "" -e $'$ a\\\n'"${psqlrcLineEscaped}" ${psqlrcFile}
 echo $'Your psqlrc configuration:\n'"$(cat ${psqlrcFile})"
 
 aliasFile="${HOME}/.bash_profile"
-aliasLine='alias updatesql="cd '\''/Users/lukaszhanusik/.support-sql-procedures'\'' && ./update.sh"'
+aliasName="updatesql"
+aliasLine='alias '"${aliasName}"'="cd '\'"${scriptDir}"\'' && ./update.sh"'
 
-## alias updatesql="cd '/Users/lukaszhanusik/.support-sql-procedures' && ./update.sh"
-
+alias updatesql="cd ${scriptDir} && ./update.sh"
 grep -qxF "${aliasLine}" ${aliasFile} || sed -i "" -e $'$ a\\\n'"${aliasLine}" ${aliasFile}
-echo $'Your psqlrc configuration:\n'"$(cat ${aliasFile})"
+echo $'Your alias configuration:\n'"$(alias)"
