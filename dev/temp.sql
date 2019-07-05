b1edea31599fbe733b9ebcccd15557c6eacdebca
lukaszhanusik <lukasz.hanusik@trustly.com>
2019-07-05 03:32:00 +0200

    Remove unnecesairy code from temp

--- dev/temp.sql
+++ dev/temp.sql
@@ -1,22 +1,9 @@
-\cd :local_path_supportsqlprocedures
+-- TEMP Examples of copy
+-- https://www.postgis.us/presentations/PGOpen2018_data_loading.pdf
 
-DROP TABLE IF EXISTS supportsqlprocedures_procedures;
-CREATE TEMP TABLE supportsqlprocedures_procedures(filename text);
 -- \COPY supportsqlprocedures_procedures FROM PROGRAM 'find /Users/lukaszhanusik/.support-sql-procedures/procedures -maxdepth 1 -type f -printf "%f\n"';
-\COPY supportsqlprocedures_procedures FROM PROGRAM 'ls procedures/';
-
-SELECT * FROM supportsqlprocedures_procedures;
-
-
-\cd :local_path_supportsqlprocedures/procedures
-DROP TABLE IF EXISTS SupportSQL_Procedures;
-CREATE TEMP TABLE SupportSQL_Procedures(Filename text, Comment text);
-
 -- \COPY supportsqlprocedures_procedures_details FROM PROGRAM 'for f in *.sql; do <"$f" read line; printf "$f,$line\n"; done' WITH (format 'csv');
-\COPY supportsqlprocedures_procedures_details FROM PROGRAM 'for f in *.sql; do <"$f" read line; printf "$f;$line\n"; done' DELIMITER ';';
-
-
-
+-- \COPY supportsqlprocedures_procedures_details FROM PROGRAM 'for f in *.sql; do <"$f" read line; printf "$f;$line\n"; done' DELIMITER ';';
 
 -- head -1 procedures/* | awk 'NR > 1 { print $9 ":" $1 }'
 -- awk "NR==1{print}" procedures/*
