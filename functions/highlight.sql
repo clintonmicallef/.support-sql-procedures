950e1f6ee47f1350799473acfbc9762dbc19532f
lukaszhanusik <lukasz.hanusik@trustly.com>
2019-07-05 03:03:33 +0200

    Add function to color selected input

---
+++ functions/highlight.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION pg_temp.highlight(_text text)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+_ESCReset text := E'\x1b[0m';
+_ESCColor text := E'\x1b[32m'; -- green
+BEGIN
+RAISE NOTICE E'%', _ESCColor || _Text || _ESCReset;
+RETURN;
+END;
+$function$;
