e7e5b29997b660b1ad36d7d4093b14d25501c6cc
Benjaminschembri <benjamin.schembri@trustly.com>
2019-07-05 13:11:12 +0200

    Add context to install_troubleshoot.sh and execute mode

---
+++ install_troubleshoot.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+scriptDir=$(cd $(dirname "$0"); pwd)
+
+psqlrcFile="${HOME}/.psqlrc"
+psqlrcLine="\i '${scriptDir}/init.psql'"
+psqlrcLineEscaped="\\${psqlrcLine}"
+psqlSetLocalPath="\set local_path_supportsqlprocedures '${scriptDir}'"
+psqlSetLocalPathEscaped="\\${psqlSetLocalPath}"
+
+echo $psqlrcFile
+echo $psqlrcLine
+echo $psqlrcLineEscaped
+echo $psqlSetLocalPath
+echo $psqlSetLocalPathEscaped
+
+touch ${psqlrcFile}
+grep -qxF "${psqlSetLocalPath}" ${psqlrcFile} || sed -i "" -e $'$ a\\\n'"${psqlSetLocalPathEscaped}" ${psqlrcFile}
+grep -qxF "${psqlrcLine}" ${psqlrcFile} || sed -i "" -e $'$ a\\\n'"${psqlrcLineEscaped}" ${psqlrcFile}
+echo $'Your psqlrc configuration:\n'"$(cat ${psqlrcFile})"
+
+aliasFile="${HOME}/.bash_profile"
+aliasName="updatesql"
+aliasLine='alias '"${aliasName}"'="cd '\'"${scriptDir}"\'' && ./update.sh"'
+
+alias updatesql="cd ${scriptDir} && ./update.sh"
+grep -qxF "${aliasLine}" ${aliasFile} || sed -i "" -e $'$ a\\\n'"${aliasLine}" ${aliasFile}
+echo $'Your alias configuration:\n'"$(alias)"
