33a0d1c71bd2ad5f241332abbce9c61b3e3a64ea
lukaszhanusik <lukasz.hanusik@trustly.com>
2019-07-05 13:39:23 +0200

    Add comments to intall.sh and update troubleshooting

--- install.sh
+++ install.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 scriptDir=$(cd $(dirname "$0"); pwd)
 
+# Set variables to add with new lines to .psqlrc
 psqlrcFile="${HOME}/.psqlrc"
 psqlrcLine="\i '${scriptDir}/init.psql'"
 psqlrcLineEscaped="\\${psqlrcLine}"
 psqlSetLocalPath="\set local_path_supportsqlprocedures '${scriptDir}'"
 psqlSetLocalPathEscaped="\\${psqlSetLocalPath}"
 
+# Create ~/.psqlrc file if does not exist. Add below lines at the end of the file
 touch ${psqlrcFile}
+
+# Add line \set local_path_supportsqlprocedures '${scriptDir}'
 grep -qxF "${psqlSetLocalPath}" ${psqlrcFile} || sed -i "" -e $'$ a\\\n'"${psqlSetLocalPathEscaped}" ${psqlrcFile}
+
+# Add line \i '${scriptDir}/init.psql'
 grep -qxF "${psqlrcLine}" ${psqlrcFile} || sed -i "" -e $'$ a\\\n'"${psqlrcLineEscaped}" ${psqlrcFile}
+
+# Print your configuration
 echo $'Your psqlrc configuration:\n'"$(cat ${psqlrcFile})"
 
+# Set variables to add with new lines to .bash_profile
 aliasFile="${HOME}/.bash_profile"
 aliasName="updatesql"
 aliasLine='alias '"${aliasName}"'="cd '\'"${scriptDir}"\'' && ./update.sh"'
 
+# Set alias updatesql
 alias updatesql="cd ${scriptDir} && ./update.sh"
+
+# Add line alias updatesql="cd ${scriptDir} && ./update.sh"
 grep -qxF "${aliasLine}" ${aliasFile} || sed -i "" -e $'$ a\\\n'"${aliasLine}" ${aliasFile}
+
+# Print your configuration
 echo $'Your alias configuration:\n'"$(alias)"
--- install_troubleshoot.sh
+++ install_troubleshoot.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
 scriptDir=$(cd $(dirname "$0"); pwd)
 
+# Set variables to add with new lines to .psqlrc
 psqlrcFile="${HOME}/.psqlrc"
 psqlrcLine="\i '${scriptDir}/init.psql'"
 psqlrcLineEscaped="\\${psqlrcLine}"
 psqlSetLocalPath="\set local_path_supportsqlprocedures '${scriptDir}'"
 psqlSetLocalPathEscaped="\\${psqlSetLocalPath}"
 
-echo $psqlrcFile
-echo $psqlrcLine
-echo $psqlrcLineEscaped
-echo $psqlSetLocalPath
-echo $psqlSetLocalPathEscaped
+# Display all variables
+echo "psqlrcFile: ${psqlrcFile}"
+echo "psqlrcLine: ${psqlrcLine}"
+echo "psqlrcLineEscaped: ${psqlrcLineEscaped}"
+echo "psqlSetLocalPath: ${psqlSetLocalPath}"
+echo "psqlSetLocalPathEscaped: ${psqlSetLocalPathEscaped}"
 
+# Create ~/.psqlrc file if does not exist. Add below lines at the end of the file
 touch ${psqlrcFile}
+
+# Add line \set local_path_supportsqlprocedures '${scriptDir}'
 grep -qxF "${psqlSetLocalPath}" ${psqlrcFile} || sed -i "" -e $'$ a\\\n'"${psqlSetLocalPathEscaped}" ${psqlrcFile}
+
+# Add line \i '${scriptDir}/init.psql'
 grep -qxF "${psqlrcLine}" ${psqlrcFile} || sed -i "" -e $'$ a\\\n'"${psqlrcLineEscaped}" ${psqlrcFile}
+
+# Print your configuration
 echo $'Your psqlrc configuration:\n'"$(cat ${psqlrcFile})"
 
+# Set variables to add with new lines to .bash_profile
 aliasFile="${HOME}/.bash_profile"
 aliasName="updatesql"
 aliasLine='alias '"${aliasName}"'="cd '\'"${scriptDir}"\'' && ./update.sh"'
 
-alias updatesql="cd ${scriptDir} && ./update.sh"
-grep -qxF "${aliasLine}" ${aliasFile} || sed -i "" -e $'$ a\\\n'"${aliasLine}" ${aliasFile}
-echo $'Your alias configuration:\n'"$(alias)"
+# Display all variables
+echo "aliasFile: ${aliasFile}"
+echo "aliasName: ${aliasName}"
+echo "aliasLine: ${aliasLine}"
 
-#Add
+# Set alias updatesql
+alias updatesql="cd ${scriptDir} && ./update.sh"
+
+# Add line alias updatesql="cd ${scriptDir} && ./update.sh"
+grep -qxF "${aliasLine}" ${aliasFile} || sed -i "" -e $'$ a\\\n'"${aliasLine}" ${aliasFile}
+
+# Print your configuration
+echo $'Your alias configuration:\n'"$(alias)"
