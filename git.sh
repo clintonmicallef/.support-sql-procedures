90ca423e140037e6927d80f38afdeaa95acb6df2
lukaszhanusik <lukasz.hanusik@trustly.com>
2019-07-06 03:55:59 +0200

    Add shell script commands to interactively update Git config. Add shell script commands to pull Git repo.

--- git.sh
+++ git.sh
@@ -1,28 +1,57 @@
 #!/bin/bash
+scriptDir=$(cd $(dirname "$0"); pwd)
+
 # Configure Git local parameters
 gitUserEmail=`id -F | iconv -f utf8 -t ascii//TRANSLIT | awk '{print tolower($1)"."tolower($2)"@trustly.com"}'`
 gitUserName=`id -F | iconv -f utf8 -t ascii//TRANSLIT | awk '{print $1,$2}'`
 
-echo ${gitUserEmail}
-echo ${gitUserName}
-# git config --global user.email "YOUR_TRUSTLY_EMAIL@trustly.com"
-# git config --global user.name "YOUR_FIRSTNAME YOUR_LASTNAME"
-# git config --list
+# Declare interactive function to set git config
+git_config()
+{
+  gitUserEmail=$1
+  gitUserName=$2
+  echo "user.email ${gitUserEmail}"
+  echo "user.name ${gitUserName}"
+  read -p "Do you want to configure your Git with following parameters: '${gitUserEmail}' (${gitUserName}) ? [Yes/No] " answer
 
-scriptDir=$(cd $(dirname "$0"); pwd)
+  while true
+  do
+    case $answer in
+     [yY]* ) echo "Git configuration updated"
+             git config user.email "${gitUserEmail}"
+             git config user.name "${gitUserName}"
+             break;;
 
-# (1) prompt user, and read command line argument
-read -p "Do you want to update the following repository: '${scriptDir}' ? [Yes/No] " answer
+     [nN]* ) echo "Git configuration unchanged"
+             exit;;
 
-# (2) handle the command line argument we were given
-while true
-do
-  case $answer in
-   [yY]* ) echo "Okay, will do"
-           break;;
+     * )     echo "Expecting Yes or No....";
+             break ;;
+    esac
+  done
+}
 
-   [nN]* ) exit;;
+# Declare interactive function to git pull
+git_update()
+{
+  scriptDir=$1
+  echo "current path: ${scriptDir}"
+  read -p "Do you want to fetch a following repository: '${scriptDir}' ? [Yes/No] " answer
 
-   * )     echo "Dude, just enter Y or N, please."; break ;;
-  esac
-done
+  while true
+  do
+    case $answer in
+     [yY]* ) echo "Updating...."
+             /.update.sh
+             break;;
+
+     [nN]* ) echo "Change active directory to Git repository to fetch"
+             exit;;
+
+     * )     echo "Expecting Yes or No....";
+             break ;;
+    esac
+  done
+}
+
+git_config ${gitUserEmail} ${gitUserName}
