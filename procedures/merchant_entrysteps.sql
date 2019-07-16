febaa438d4624f50c7c7a8b58348f6bef884d45e
Benjaminschembri <benjamin.schembri@trustly.com>
2019-07-16 16:23:40 +0200

    Add Get_Account_Selector(EntrySteps.EntryStepID, Users.UserID)) to Merchant_Entrysteps for proper filtering. #317032

--- procedures/merchant_entrysteps.sql
+++ procedures/merchant_entrysteps.sql
@@ -54,7 +54,10 @@ SELECT DistinctEntryStepIdentifiers.Username,
         WHERE Users.Username = :'processingaccount' --CHANGE USERNAME
           AND (EntrySteps.Disabled IS NULL OR users.username = 'apitest')
           AND (EntrySteps.Unlisted IS FALSE OR users.username = 'apitest')
-          AND (CASE WHEN UserSettings.RequireFetchAccountFromBank THEN EntrySteps.Name != 'Other bank' ELSE NULL END)
+          AND (
+            NOT (UserSettings.RequireFetchAccountFromBank)
+             OR EXISTS(SELECT 1 FROM Get_Account_Selector(EntrySteps.EntryStepID, Users.UserID))
+           )--(CASE WHEN UserSettings.RequireFetchAccountFromBank THEN EntrySteps.Name != 'Other bank' ELSE NULL END)
         ORDER BY EntrySteps.Identifier, COALESCE(UserEntrySteps.Allow::boolean, UserEntryStepCountries.UserEntryStepCountryID IS NOT NULL) DESC,
               (CASE WHEN UserEntrySteps.UserEntryStepID IS NOT NULL THEN 1
                     WHEN UserEntryStepCountries.UserEntryStepCountryID IS NOT NULL THEN 2
