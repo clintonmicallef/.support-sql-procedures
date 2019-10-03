0ca698fdcd211c4c19eac76f404b2add15616341
Benjaminschembri <benjamin.schembri@trustly.com>
2019-10-03 09:53:27 +0200

    Add Integration Authentication method when checking end user device details per OrderID

--- procedures/enduser_device_info.sql
+++ procedures/enduser_device_info.sql
@@ -7,9 +7,12 @@
 SELECT OrderFingerPrints.OrderID,
        enduserclientplatforms.*,
        browseruseragents.value as useragent,
+       IntegrationAuthenticationMethods.Name AS LoginAuthenticationMethod,
        FingerPrints.lastseen
   FROM EnduserClientPlatforms
   JOIN FingerPrints ON (FingerPrints.enduserclientplatformid = EnduserClientPlatforms.enduserclientplatformid)
   LEFT JOIN browseruseragents ON BrowserUserAgents.BrowserUserAgentID = FingerPrints.BrowserUserAgentID
   JOIN OrderFingerPrints ON (OrderFingerPrints.fingerprintid = FingerPrints.fingerprintid)
+  JOIN Orders ON (Orders.OrderID = OrderFingerPrints.OrderID)
+  LEFT JOIN IntegrationAuthenticationMethods ON (IntegrationAuthenticationMethods.IntegrationAuthenticationMethodID = Orders.IntegrationAuthenticationMethodID)
  WHERE OrderFingerPrints.OrderID = :'orderid';
