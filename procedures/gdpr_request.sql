c27519d3b918d82ffd1f38ec77ef3fbd18399a02
Benjaminschembri <benjamin.schembri@trustly.com>
2019-12-10 16:41:43 +0100

    final check

--- procedures/gdpr_request.sql
+++ procedures/gdpr_request.sql
@@ -47,12 +47,10 @@ WITH Data AS (
     LEFT JOIN kyc.PnpOrders ON (kyc.PnpOrders.OrderID = OrdersCollection.OrderID)
     LEFT JOIN kyc.OrdersEntity  ON (kyc.OrdersEntity.OrderID = OrdersCollection.OrderID)
     LEFT JOIN kyc.Entities ON (kyc.Entities.KYCEntityID = KYC.OrdersEntity.KYCEntityID)
-   WHERE Orders.OrderID = :'orderID'
-
-      /*   (SELECT CASE WHEN NULLIF(:'person','') IS NOT NULL THEN :'person' = ANY(TransferBankAccounts.personIDs)
+   WHERE (SELECT CASE WHEN NULLIF(:'person','') IS NOT NULL THEN :'person' = ANY(TransferBankAccounts.personIDs)
                       WHEN NULLIF(:'transferbankaccount','') IS NOT NULL THEN TransferBankAccounts.TransferBankAccountID = :'transferbankaccount'
                       WHEN NULLIF(:'bankaccountnumber','') IS NOT NULL THEN TransferBankAccounts.Accountnumber = :'bankaccountnumber'
-                      ELSE NULL END))*/
+                      WHEN (NULLIF(:'orderID',''))::bigint IS NOT NULL THEN Orders.OrderID = (:'orderID')::bigint ELSE NULL END)
  )
  SELECT DISTINCT
         INITCAP(replace(replace(replace(replace(DATA.name::text,'{',''),'}',''),'"',''),',NULL','')) AS name,
