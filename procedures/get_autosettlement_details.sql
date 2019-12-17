32ca7bf3f84ac54479c7a5368f61d40db83c1c8b
Benjaminschembri <benjamin.schembri@trustly.com>
2019-12-17 15:55:08 +0100

    fix typo

--- procedures/get_autosettlement_details.sql
+++ procedures/get_autosettlement_details.sql
@@ -236,4 +236,4 @@ WITH PARAMETERS(processingaccount) AS(
 
 
 INSERT INTO SupportSQL_UserLogExport VALUES (user, now(), 'get_autosettlement_details.sql');
-\COPY (SELECT * FROM SupportSQL_UserLogExpoort) TO PROGRAM 'cat >> /Volumes/GoogleDrive/Shared\ drives/Support/useraccesslog.csv' CSV
+\COPY (SELECT * FROM SupportSQL_UserLogExport) TO PROGRAM 'cat >> /Volumes/GoogleDrive/Shared\ drives/Support/useraccesslog.csv' CSV
