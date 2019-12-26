d5f20ab5f166851faef6f3ebcb86f62d6b3be093
Benjaminschembri <benjamin.schembri@trustly.com>
2019-12-26 16:11:15 +0100

    improve procedure to obtain all possible end user orders per merchant

--- procedures/all_enduser_activity.sql
+++ procedures/all_enduser_activity.sql
@@ -8,68 +8,59 @@
 \pset expanded off
 
 WITH EnduserOrders AS(
- SELECT BankOrders.OrderID, Users.Username, BankOrders.Balance, BankOrders.PersonID
-   FROM BankOrders
-   JOIN Orders ON Orders.OrderID = BankOrders.OrderID
-   JOIN Users ON Users.UserID = Orders.UserID
-  WHERE TransferBankAccountID IN (:transferbankaccountID)
+  SELECT DISTINCT BankOrders.OrderID, Users.Username, TransferBankAccountID
+    FROM BankOrders
+    JOIN Orders ON Orders.OrderID = BankOrders.OrderID
+    JOIN Users ON Users.UserID = Orders.UserID
+   WHERE TransferBankAccountID = :'transferbankaccountID'
+     AND (SELECT CASE WHEN NULLIF(:'processingaccount','') IS NOT NULL THEN Users.Username = :'processingaccount' ELSE 'TRUE' END)
+   UNION
+  SELECT DISTINCT OrderBankAccounts.OrderID, Users.Username, TransferBankAccountID
+    FROM orderbankaccounts
+    JOIN Orders ON Orders.OrderID = OrderBankAccounts.OrderID
+    JOIN Users ON Users.UserID = Orders.UserID
+   WHERE TransferBankAccountID = :'transferbankaccountID'
+     AND (SELECT CASE WHEN NULLIF(:'processingaccount','') IS NOT NULL THEN Users.Username = :'processingaccount' ELSE 'TRUE' END)
+   UNION
+  SELECT DISTINCT OrderID, Users.Username, TransferBankAccountID
+    FROM accountselectortransfers
+    JOIN Users ON Users.UserID = accountselectortransfers.UserID
+   WHERE TransferBankAccountID = :'transferbankaccountID'
     AND (SELECT CASE WHEN NULLIF(:'processingaccount','') IS NOT NULL THEN Users.Username = :'processingaccount' ELSE 'TRUE' END)
-  UNION
- SELECT OrderBankAccounts.OrderID, Users.Username, NULL::bigint, 'NULL'::text
-   FROM OrderBankAccounts
-   JOIN Orders ON Orders.OrderID = OrderBankAccounts.OrderID
-   JOIN Users ON Users.UserID = Orders.UserID
-  WHERE TransferBankAccountID IN (:transferbankaccountID)
-    AND (SELECT CASE WHEN NULLIF(:'processingaccount','') IS NOT NULL THEN Users.Username = :'processingaccount' ELSE 'TRUE' END)
-  ),
-  MerchantCreditedOrders AS(
-    SELECT superuser AS SuperUser,
-           datestamp AS Datestamp
-      FROM vUserAccessLog
-     WHERE function = 'Send_Credit'
-       AND datestamp IN (SELECT triggeredcredit FROM Transfers WHERE TransferTypeID = 1 AND Transfers.OrderID IN (SELECT DISTINCT OrderID FROM EnduserOrders))
-     ),
-     TrustlyCreditedOrders AS(
-       SELECT OrderID,
-              FixedBy
-         FROM FixedOrders
-        WHERE OrderID IN  (SELECT DISTINCT OrderID FROM EnduserOrders)
-      )
-      SELECT Users.Username AS ProcessingAccount,
-             OrdersKYCData.Name AS Enduser_name,
-             EnduserOrders.PersonID,
-             Transfers.OrderID AS OrderID,
-             Orders.EntrystepID,
-             TransferTypes.name AS TransferType,
-             Transfers.datestamp::timestamp(0) AS Initiated_On,
-             TransferStates.Name AS TransferState,
-             Transfers.ModificationDate::timestamp(0),
-             Transfers.Amount,
-             EnduserOrders.Balance,
-             Transfers.Currency,
-             (CASE WHEN Transfers.TriggeredCredit IS NOT NULL THEN COALESCE(MerchantCreditedOrders.SuperUser, TrustlyCreditedOrders.fixedby, 'System'::text) ELSE NULL END) AS Credited_By,
-             Transfers.TriggeredCredit::timestamp(0) AS triggered_credit,
-             Transfers.TriggeredDebit::timestamp(0) AS Triggered_debit,
-             Transfers.TriggeredRefund::timestamp(0) AS Triggered_refund,
-             Transfers.TriggeredSettle::timestamp(0) AS Triggered_settled,
-             ROUND(ExceededExposureLimits.consumedlimit,2) AS Consumedimit,
-             ROUND(ExceededExposureLimits.maxlimit,2) AS MaxLimit,
-             decisionlog.Reason AS Risk_Log,
-             array_agg(Orders.EnduserID) AS EnduserID
-        FROM Orders
-        JOIN EnduserOrders ON EnduserOrders.OrderID = Orders.OrderID
-        LEFT JOIN OrdersKYCData ON OrdersKYCData.OrderID = Orders.OrderID
-        JOIN Transfers ON Transfers.OrderID = Orders.OrderID AND Transfers.TransferTypeID IN (1,2)
-        JOIN TransferStates ON TransferStates.TransferStateID = Transfers.TransferStateID
-        JOIN TransferTypes ON TransferTypes.TransferTypeID = transfers.TransferTypeID
-        JOIN Users ON Users.UserID = Transfers.UserID
-        LEFT JOIN MerchantCreditedOrders ON MerchantCreditedOrders.datestamp = transfers.triggeredcredit
-        LEFT JOIN TrustlyCreditedOrders ON TrustlyCreditedOrders.OrderID = Orders.OrderID
-        LEFT JOIN risk.decisionlog ON risk.decisionlog.OrderID = Orders.OrderID
-        LEFT JOIN ExceededExposureLimits ON ExceededExposureLimits.OrderID = Orders.OrderID
-       WHERE Orders.OrderID IN (SELECT OrderID FROM EnduserOrders)
-       GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
-       ORDER BY 7 DESC
+   UNION
+  SELECT DISTINCT Transfers.OrderID, Users.Username, TransferBankAccounts.TransferBankAccountID
+    FROM autogiro.payments
+    JOIN Users ON Users.UserID = autogiro.payments.UserID
+    JOIN Transfers ON Transfers.TransferID = payments.TransferID
+    JOIN autogiro.payers ON payments.payerid = payers.payerid
+    JOIN TransferBankAccounts ON TransferBankAccounts.AccountID = autogiro.Payers.AccountID
+    WHERE TransferBankAccounts.TransferBankAccountID = :'transferbankaccountID'
+     AND (SELECT CASE WHEN NULLIF(:'processingaccount','') IS NOT NULL THEN Users.Username = :'processingaccount' ELSE 'TRUE' END)
+   UNION
+  SELECT DISTINCT PnpOrders.OrderID, Users.username, TransferBankAccountID
+    FROM KYC.PnpOrders
+    JOIN Orders ON Orders.OrderID = PnpOrders.OrderID
+    JOIN Users ON Users.UserID = Orders.UserID
+    JOIN KYC.bankentitiesaccounts ON kyc.bankentitiesaccounts.BankEntityID = KYC.PNPOrders.BankEntityID
+   WHERE TransferBankAccountID = :'transferbankaccountID'
+     AND (SELECT CASE WHEN NULLIF(:'processingaccount','') IS NOT NULL THEN Users.Username = :'processingaccount' ELSE 'TRUE' END)
+  )
+      SELECT EnduserOrders.OrderID,
+             EnduserOrders.Username AS ProcessingAccount,
+             COALESCE(OrdersKYCData.Name, TransferBankAccounts.Name) AS Enduser_name,
+             Orders.EnduserID,
+             TransferBankAccounts.PersonID,
+             WorkerTypes.name AS OrderType,
+             Orders.datestamp::timestamp(0) AS Initiated_On,
+             Orders.APIAmount,
+             TransferBankaccounts.Balance,
+             Orders.APICurrency
+        FROM EnduserOrders
+        JOIN Orders ON EnduserOrders.OrderID = Orders.OrderID
+        JOIN TransferBankAccounts ON TransferBankAccounts.TransferBankAccountID = EnduserOrders.transferbankaccountID
+        LEFT JOIN OrdersKYCData ON OrdersKYCData.OrderID = EnduserOrders.OrderID
+        LEFT JOIN Workertypes ON Workertypes.WorkertypeID = Orders.InitOrderTypeID
+       ORDER BY Orders.datestamp ASC
 ;
 
 -- Inserts data of this execution in temp table. Copy this data into GoogleDrive. Copy from GoogleDrive ALL data back into another temp table.
