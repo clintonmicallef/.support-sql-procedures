8df3d009b82e50c23363e7b55444f03b63b7d7f8
Benjaminschembri <benjamin.schembri@trustly.com>
2020-07-07 17:20:15 +0200

    Fixing scenarios for queued refunds prior to settlement and adding addditional data for cancelled interrupted orders and settled failed orders

--- procedures/check_deposit_refund.sql
+++ procedures/check_deposit_refund.sql
@@ -18,30 +18,48 @@ WITH Parameters AS(
  )
  SELECT (CASE WHEN APICalls.APICallID IS NOT NULL THEN 'Merchant Refund via API' ELSE NULL END) AS Case,
         datestamp AS Datestamp,
-        APICalls.APICallID::text AS Data
+        'apicallID ' || APICalls.APICallID::text  AS Data
    FROM APICalls
   WHERE Method = 'Refund' AND
         Username IN (SELECT Username FROM Parameters) AND
-        Datestamp BETWEEN ((SELECT DebitDatestamp FROM Parameters) - interval '10 mins') AND ((SELECT DebitDatestamp FROM Parameters) + interval '10 mins') AND
-        (SignedResult::json->'data'->>'orderid')::bigint = (SELECT OrderID FROM Parameters)::bigint
+        Datestamp BETWEEN (SELECT Datestamp FROM Parameters) AND ((SELECT DebitDatestamp FROM Parameters) + interval '10 mins') AND
+        (((SignedResult::json->'data'->>'orderid')::bigint = (SELECT OrderID FROM Parameters)::bigint) OR ((resultdata::json->>'orderid')::bigint = (SELECT OrderID FROM Parameters)::bigint))
   UNION
  SELECT (CASE WHEN TRUE THEN 'Merchant Refund via Backoffice' ELSE NULL END) AS Case,
         datestamp AS Datestamp,
-        superuser::text AS Data
+        'superuser ' || superuser::text AS Data
    FROM vUserAccessLog
   WHERE function = 'Manual_Refund'
     AND datestamp::timestamp(0) IN (SELECT DebitDatestamp::timestamp(0) FROM Parameters)
   UNION
- SELECT (CASE WHEN transferstatetransitions.TransferID IS NOT NULL
-              THEN 'Refund due to Settlement after Transfer has been failed'
-              ELSE 'System Cancel - Refund upon Settlement' END) AS Case,
-        Transfers.TriggeredRefund AS Datestamp,
-        COALESCE(transferstatetransitions.TransferID, Transfers.TransferID)::text AS Data
+ SELECT (CASE WHEN TRUE THEN 'Merchant Refund via Backoffice' ELSE NULL END) AS Case,
+        queuedrefunds.datestamp AS Datestamp,
+        'superuser ' || superuser::text AS Data
+   FROM queuedrefunds
+   JOIN vuserAccessLog ON  vuserAccessLog.datestamp = queuedrefunds.datestamp
+  WHERE vuserAccessLog.function = 'Manual_Refund'
+    AND queuedrefunds.orderid = (SELECT OrderID FROM Parameters)
+  UNION
+ SELECT (CASE WHEN TRUE THEN 'Refund Settled Interrupted Order - System cancel' ELSE NULL END),
+        DepositRefundQueue.DAtestamp,
+        'refund_date ' ||DepositRefundQueue.Refunded::timestamp(0)::text AS DAta
    FROM Transfers
-   LEFT JOIN transferstatetransitions ON transferstatetransitions.TransferID = Transfers.TransferID AND transferstatetransitions.ToTransferStateID = 6
+   JOIN DepositRefundQueue ON DepositRefundQueue.TransferID = Transfers.TransferID
   WHERE Transfers.TransferTypeID = 1
-    AND Transfers.TriggeredRefund IS NOT NULL
+    AND Transfers.TriggeredDebit IS NULL --DO not confuse with failed orders
+    AND Transfers.orderID = (SELECT OrderID FROM Parameters)
+  UNION
+ SELECT (CASE WHEN TRUE THEN 'Refund due to Failed deposit now Settled and OK to Debit notification'
+              ELSE 'Not Found! Report to Administrator' END) AS Case,
+        Transfers.TriggeredRefund AS Datestamp,
+        'Failed_date ' || Transfers.triggereddebit::timestamp(0)::text AS Data
+   FROM Transfers
+   JOIN transferstatetransitions ON transferstatetransitions.TransferID = Transfers.TransferID AND transferstatetransitions.ToTransferStateID = 8
+  WHERE Transfers.TransferTypeID = 1
+    AND Transfers.TriggeredDebit IS NOT NULL
+    AND Transfers.TransferSTateID = 12
     AND Transfers.OrderID IN (SELECT OrderID FROM Parameters)
+    AND EXISTS(SELECT 1 from transfers refundwithdrawal where refundwithdrawal.transfertypeid = 2 and refundwithdrawal.orderid=transfers.orderid)
 ;
 
 
