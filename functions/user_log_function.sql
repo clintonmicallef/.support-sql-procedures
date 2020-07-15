eb7da321374bf57404139b1ac93fdd6d372a67ba
Benjaminschembri <benjamin.schembri@trustly.com>
2020-07-15 12:45:22 +0200

    try to quiet function part 2

--- functions/user_log_function.sql
+++ functions/user_log_function.sql
@@ -1,5 +1,4 @@
 /* Function used to copy user activity run by every procedures in repo, into an export temp table (making sure table is clean before not to copy duplicates from previous activity) */
-
 CREATE OR REPLACE FUNCTION pg_temp.user_log_function(_username text, _datestamp timestamp, _procedure text)
   RETURNS void
   LANGUAGE plpgsql
