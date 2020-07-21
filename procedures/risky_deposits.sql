a58bfd6aab10e250350a1ec83e8236bdb50dd657
Benjaminschembri <benjamin.schembri@trustly.com>
2020-07-21 12:32:45 +0200

    functionality for 2nd line performing risky deposits procedures

---
+++ procedures/risky_deposits.sql
@@ -0,0 +1,26 @@
+/* 2nd Line procedure to query risky deposits and fail according to input by 2nd line agent. Note that validation for access privelages done in risky_deposots() function*/
+
+\set QUIET ON
+
+\pset pager off
+
+\pset expanded off
+
+\echo 'Loading risky deposits...'
+
+SELECT * FROM pg_temp._risky_deposits(NULL);
+
+\prompt 'do you want to fail these deposits?', answer
+\echo ''
+\prompt 'are you sure?', answerfinal
+\echo ''
+
+SELECT * FROM pg_temp._risky_deposits(:'answerfinal');
+
+\pset pager on
+
+-- Inserts data of this execution in temp table. Copy this data into GoogleDrive. Copy from GoogleDrive ALL data back into another temp table for viewing.
+\t
+SELECT pg_temp.user_log_function(user::text, now()::timestamp , 'risky_deposits');
+\t
+\i '~/.support-sql-procedures/userlogsetup.psql'
