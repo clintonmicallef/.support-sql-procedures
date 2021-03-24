76c809affeb11d8368e3f960d9933e841f243757
Benjaminschembri <benjamin.schembri@trustly.com>
2021-03-24 11:42:10 +0100

    case when updated

--- procedures/risky_deposits.sql
+++ procedures/risky_deposits.sql
@@ -6,9 +6,8 @@
 
 SELECT * FROM pg_temp.secondline_view_risky_deposits_upd();
 
-SELECT (CASE WHEN user IN ('tomasvebr', 'benjaminschembri', 'dimitriossliakas') THEN 'do you want to fail these deposits? [yes/no]' ELSE 'Unauthorised Access - 2nd line access only' END);
+SELECT (CASE WHEN user IN ('tomasvebr', 'benjaminschembri', 'dimitriossliakas') THEN 'To Fail these deposits, run function: pg_temp.secondline_fail_risky_deposits()' ELSE NULL END);
 
-SELECT
 
 -- Inserts data of this execution in temp table. Copy this data into GoogleDrive. Copy from GoogleDrive ALL data back into another temp table for viewing.
 \t
