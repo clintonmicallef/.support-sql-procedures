fa067316cba6becf3652f6a555eef1b629e609b5
Benjaminschembri <benjamin.schembri@trustly.com>
2021-10-01 19:11:38 +0200

    Try to improve running time and make results more concise

--- procedures/monitor_all_withdrawals.sql
+++ procedures/monitor_all_withdrawals.sql
@@ -10,7 +10,7 @@ SELECT BankAccounts.BankAccountID,
        JWOWWInstances.Crashed AS CrashedJWOWWs,
        JWOWWInstances.LastExecute,
        --JWOWWInstances.StatusTimestamp,
-       JWOWWInstances.LastLedger,
+       --JWOWWInstances.LastLedger,
        MIN(BankWithdrawals.datestamp)::timestamp(0),
        MAX(BankWithdrawals.datestamp)::timestamp(0),
        BankWithdrawals.Currency AS Curr,
@@ -18,36 +18,36 @@ SELECT BankAccounts.BankAccountID,
        count(*),
        Delays.Count AS Delayed,
        Delays.MAXDELAY AS MaxDelay,
-       COALESCE(BankAccountBalances.Available,BankAccountBalances.Balance) AS BankBalance,
-       IncomingFundings.Sum AS Incomingfunds,
-       sum(sum(BankWithdrawals.Amount)) OVER(PARTITION BY BankAccounts.BankAccountID) AS ReqBalance,
+       --COALESCE(BankAccountBalances.Available,BankAccountBalances.Balance) AS BankBalance,
+       --IncomingFundings.Sum AS Incomingfunds,
+       --sum(sum(BankWithdrawals.Amount)) OVER(PARTITION BY BankAccounts.BankAccountID) AS ReqBalance,
        sum(BankWithdrawals.Amount) AS Sum
        --(CASE WHEN Delays.MAXDELAY IS NOT NULL AND Delays.MAXDELAY > '15 MINS'::interval THEN 'DELAY!' ELSE NULL END) AS Alert
   FROM BankWithdrawals
   JOIN BankWithdrawalStates ON (BankWithdrawalStates.BankWithdrawalStateID = BankWithdrawals.BankWithdrawalStateID)
   JOIN BankWithdrawalTypes ON (BankWithdrawalTypes.BankWithdrawalTypeID = BankWithdrawals.BankWithdrawalTypeID)
   JOIN BankAccounts ON (BankAccounts.BankAccountID = BankWithdrawals.SendingBankAccountID)
-  LEFT JOIN BankAccountBalances ON (BankAccountBalances.BankAccountID = BankAccounts.BankAccountID)
-  LEFT JOIN ( SELECT InternalTransfers.ToBankAccountID,
-                     InternalTransfers.Currency,
-                     sum(InternalTransfers.Amount)
-                FROM InternalTransfers
-                JOIN BankAccounts FromBankAccounts ON (FromBankAccounts.BankAccountID = InternalTransfers.FromBankAccountID)
-                JOIN BankAccounts ToBankAccounts ON (ToBankAccounts.BankAccountID = InternalTransfers.ToBankAccountID)
-                LEFT JOIN BankNumbers FromBankNumbers ON (FromBankNumbers.BankNumberID = FromBankAccounts.BankNumberID)
-                LEFT JOIN BankNumbers ToBankNumbers ON (ToBankNumbers.BankNumberID = ToBankAccounts.BankNumberID)
-               WHERE (InternalTransfers.Executed >= current_date - 4)
-                 AND InternalTransfers.Settled IS NULL
-               GROUP BY 1,2
-  ) AS IncomingFundings ON (IncomingFundings.ToBankAccountID = BankAccounts.BankAccountID)
+  --LEFT JOIN BankAccountBalances ON (BankAccountBalances.BankAccountID = BankAccounts.BankAccountID)
+  --LEFT JOIN ( SELECT InternalTransfers.ToBankAccountID,
+  --                   InternalTransfers.Currency,
+  --                   sum(InternalTransfers.Amount)
+  --              FROM InternalTransfers
+  --              JOIN BankAccounts FromBankAccounts ON (FromBankAccounts.BankAccountID = InternalTransfers.FromBankAccountID)
+  --              JOIN BankAccounts ToBankAccounts ON (ToBankAccounts.BankAccountID = InternalTransfers.ToBankAccountID)
+  --              LEFT JOIN BankNumbers FromBankNumbers ON (FromBankNumbers.BankNumberID = FromBankAccounts.BankNumberID)
+  --              LEFT JOIN BankNumbers ToBankNumbers ON (ToBankNumbers.BankNumberID = ToBankAccounts.BankNumberID)
+  --             WHERE (InternalTransfers.Executed >= current_date - 4)
+  --               AND InternalTransfers.Settled IS NULL
+  --             GROUP BY 1,2
+  --) AS IncomingFundings ON (IncomingFundings.ToBankAccountID = BankAccounts.BankAccountID)
   LEFT JOIN ( SELECT BankWithdrawals.SendingBankAccountID,
                      array_agg(DISTINCT BankIOHeartBeat.ID) AS IDs,
                      count(DISTINCT BankIOHeartBeat.ID) AS NoOfInstances,
                      array_agg(DISTINCT CASE WHEN BankIOHeartBeat.Status = 'CRASHED' THEN 'CRASHED' ELSE 'RUNNING' END) AS Statuses,
                      string_agg(DISTINCT CASE WHEN BankIOHeartBeat.Status = 'CRASHED' THEN BankIOHeartBeat.ID ELSE NULL END,',') AS Crashed,
                      count(*),
-                     max(BankWithdrawals.TimestampExecuted)::timestamp(0) AS LastExecute,
-                     max(BankIOHeartBeat.LastLedger)::timestamp(0) AS LastLedger
+                     max(BankWithdrawals.TimestampExecuted)::timestamp(0) AS LastExecute
+                     --max(BankIOHeartBeat.LastLedger)::timestamp(0) AS LastLedger
                      --max(BankIOHeartBeat.StatusTimestamp)::timestamp(0) AS StatusTimestamp
                 FROM BankWithdrawals
                 JOIN BankIOHeartBeat ON (BankIOHeartBeat.ID = (BankWithdrawals.ProcessedBy::json->>'ID'))
@@ -83,5 +83,5 @@ SELECT BankAccounts.BankAccountID,
                        AND BlackListedBankAccounts.ToAccountNumber = BankWithdrawals.ToAccountNumber
                   )
    --  BankWithdrawals.DeQueued = 1
- GROUP BY 1, 2, 3, 4, 5, 6, 9, 10, 12, 13, 14, 15
+ GROUP BY 1, 2, 3, 4, 5, 8, 9, 11, 12
  ORDER BY BankWithdrawalState DESC, count(*) DESC;
