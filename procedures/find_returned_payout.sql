ee404316cfb596d3934edce7349b6397267ba872
tomasvebr <tomas.vebr@trustly.com>
2021-12-10 12:17:37 +0100

    Edit find_returned_payout.sql

--- procedures/find_returned_payout.sql
+++ procedures/find_returned_payout.sql
@@ -2,225 +2,253 @@
 
 --EXAMPLES: 3855645276,4224908393,3704506155,2029808911,3798653319(only ledger atm)
 
-\prompt 'Enter BankWithdrawalID to find a returned payment: ' bankwithdrawalid
-\prompt 'Enter Date Range (x days): ' daterange
+\prompt '\nEnter BankWithdrawalID to find a returned payment: ' bankwithdrawalid 
 
 \set QUIET ON
-
+--For better readability \pset expanded ON. It is set back to AUTO at the end!
 \pset expanded ON
+\timing OFF
 
 \echo '\n*******************'
 \echo '*    STATEMENT    *'
 \echo '*******************\n'
-WITH tmp AS (
-   -- Add new case for each bank which generate new bankreferencenumber (CSV statement)
+
+WITH payout_info AS(
    SELECT
-      CASE
-         WHEN ecosysaccount ilike 'CLIENT_FUNDS_ESTONIA_LHVB%' AND balancedate < '2020-10-15' THEN (
-            RIGHT(SUBSTRING(array_to_string(textcolumns,','), (POSITION('AcctSvcrRef:' in array_to_string(textcolumns,','))), 31),9)
-         )
-         -- LHV changed their format on '2020-10-15'
-         WHEN ecosysaccount ilike 'CLIENT_FUNDS_ESTONIA_LHVB' AND balancedate >= '2020-10-15' THEN (
-            RIGHT(SUBSTRING(array_to_string(textcolumns,','),POSITION('InstrId:' in array_to_string(textcolumns,',')),19),10)
-         )
-         ELSE NULL
-      END AS "bankreferencenumber"
-   FROM
-      ledger.view_all_rows
-   WHERE
-      reference = :'bankwithdrawalid'
-      AND amount < 0
-   UNION
-   -- Add new case for each bank which generate new bankreferencenumber (MT940  statement)
-   SELECT
-      CASE
-         WHEN ecosysaccount ilike ('%CLIENT_FUNDS_UNITED_KINGDOM_CITI%')
-            THEN
-               CASE
-                  WHEN (POSITION('NMSC' in array_to_string(textcolumns,','))) = 0 THEN (SUBSTRING(array_to_string(textcolumns,','),(POSITION('NTRF' in array_to_string(textcolumns,','))+4),10))
-                  ELSE (SUBSTRING(array_to_string(textcolumns,','),(POSITION('NMSC' in array_to_string(textcolumns,','))+4),(POSITION('//' in array_to_string(textcolumns,','))-POSITION('NMSC' in array_to_string(textcolumns,','))-4)))
-               END
-         ELSE NULL
-      END AS "bankreferencenumber"
-   FROM
-      mt94xparser.View_All_Rows
-   WHERE
-      reference = :'bankwithdrawalid'
-      AND amount < 0
-),
-cte AS (
-   SELECT
-      w.bankwithdrawalid as "reference",
+      w.bankledgerid,
+      w.bankwithdrawalid AS "reference",
       w.sendingbankaccountid AS "bankaccountid",
-      w.datestamp::DATE AS "date",
+      w.datestamp :: DATE AS "date",
       w.currency AS "currency",
-      w.amount AS "amount",
       CASE
          WHEN (w.amount - (100 * cr.ask)) < 0 THEN 0
          ELSE (w.amount - (100 * cr.ask))
-      END AS "amountminusfee",
+      END AS "min_amount",
+      w.amount AS "max_amount",
       w.name AS "name",
       w.toaccountnumber AS "accountnumber",
-      ba.ecosysaccount AS "bank"
+      ba.ecosysaccount AS "bank",
+      CASE
+         WHEN (tba.clearinghouseid = 44) THEN '1 month' :: INTERVAL
+         ELSE '14 days' :: INTERVAL
+      END AS "search_period"
    FROM
       bankwithdrawals w
       LEFT JOIN bankaccounts ba ON ba.bankaccountid = w.sendingbankaccountid
-      LEFT JOIN currencypairs cp ON (w.currency = cp.quotecurrency AND cp.basecurrency = 'EUR')
+      LEFT JOIN transferbankaccounts tba ON tba.transferbankaccountid = w.transferbankaccountid
+      LEFT JOIN currencypairs cp ON ( w.currency = cp.quotecurrency AND cp.basecurrency = 'EUR')
       LEFT JOIN currencyexchangerates cr ON (cp.currencypairid = cr.currencypairid)
    WHERE
       bankwithdrawalid = :'bankwithdrawalid'
-)
-SELECT
-  'CSV' AS "statement",
-  ledgerrowid,
-  ecosysaccount,
-  currency,
-  amount,
-  balancedate,
-  datecolumn1 AS "valuedate",
-  textcolumns,
-  bankledgerid
-FROM
-  ledger.view_all_rows
-WHERE
-  ecosysAccount = ( SELECT "bank" FROM cte)
-  AND balanceDate BETWEEN ((SELECT "date" FROM cte)) AND ((SELECT "date" FROM cte) + :'daterange'::INTERVAL)
-  AND currency = (SELECT "currency" FROM cte)
-  AND amount BETWEEN (SELECT "amountminusfee" FROM cte) AND (SELECT "amount" FROM cte)
-  AND (
-         (array_to_string(textcolumns,',') ilike '%' || (SELECT "accountnumber" FROM cte) || '%')
-      OR (array_to_string(textcolumns,',') ilike '%' || (SELECT "name" FROM cte) || '%')
-      OR (array_to_string(textcolumns,',') ilike '%' || (SELECT "reference" FROM cte) || '%')
-      OR (array_to_string(textcolumns,',') ilike '%' || (SELECT "bankreferencenumber" FROM tmp) || '%')
-      )
-UNION
-SELECT
-   'MT940' as "statement",
-  statementlineid,
-  ecosysaccount,
-  currency,
-  amount,
-  balancedate,
-  valuedate,
-  textcolumns,
-  bankledgerid
-FROM
-  mt94xparser.View_All_Rows
-WHERE
-   ecosysAccount = ( SELECT "bank" FROM cte)
-   AND valuedate BETWEEN ((SELECT "date" FROM cte)) AND ((SELECT "date" FROM cte) + :'daterange'::INTERVAL)
-   AND currency = (SELECT "currency" FROM cte)
-   AND amount BETWEEN (SELECT "amountminusfee" FROM cte) AND (SELECT "amount" FROM cte)
-   AND (
-         (array_to_string(textcolumns,',') ilike '%' || (SELECT "accountnumber" FROM cte) || '%')
-      OR (array_to_string(textcolumns,',') ilike '%' || (SELECT "name" FROM cte) || '%')
-      OR (array_to_string(textcolumns,',') ilike '%' || (SELECT "reference" FROM cte) || '%')
-      OR (array_to_string(textcolumns,',') ilike '%' || (SELECT "bankreferencenumber" FROM tmp) || '%')
-      );
-\echo '*******************'
-\echo '*   BANK LEDGER   *'
-\echo '*******************\n'
-WITH tmp AS (
+),
+bank_reference_number AS (
+   -- Add new case for each bank which generate new bankreferencenumber (CSV statement)
    SELECT
       CASE
-         WHEN ecosysaccount ilike 'CLIENT_FUNDS_ESTONIA_LHVB%' AND balancedate < '2020-10-15' THEN (
-            RIGHT(SUBSTRING(array_to_string(textcolumns,','), (POSITION('AcctSvcrRef:' in array_to_string(textcolumns,','))), 31),9)
-         )
-         -- LHV changed their format on '2020-10-15'
-         WHEN ecosysaccount ilike 'CLIENT_FUNDS_ESTONIA_LHVB' AND balancedate >= '2020-10-15' THEN (
-            RIGHT(SUBSTRING(array_to_string(textcolumns,','),POSITION('InstrId:' in array_to_string(textcolumns,',')),19),10)
-         )
+         -- LHV ESTONIA
+         WHEN ecosysaccount ilike 'CLIENT_FUNDS_ESTONIA_LHVB%' THEN ( SUBSTRING( array_to_string(textcolumns, ','), POSITION( 'AcctSvcrRef:' IN array_to_string(textcolumns, ',') ) + 15, 30 ) )
          ELSE NULL
-      END AS "bankreferencenumber"
+      END AS "bank_reference_number"
    FROM
       ledger.view_all_rows
    WHERE
-      reference = :'bankwithdrawalid'
-      AND amount < 0
+      bankledgerid = ( SELECT bankledgerid FROM payout_info )
+
    UNION
+
+   -- Add new case for each bank which generate new bankreferencenumber (MT940  statement)
    SELECT
       CASE
-         WHEN ecosysaccount ilike ('%CLIENT_FUNDS_UNITED_KINGDOM_CITI%')
-            THEN
-               CASE
-                  WHEN (POSITION('NMSC' in array_to_string(textcolumns,','))) = 0 THEN (SUBSTRING(array_to_string(textcolumns,','),(POSITION('NTRF' in array_to_string(textcolumns,','))+4),10))
-                  ELSE (SUBSTRING(array_to_string(textcolumns,','),(POSITION('NMSC' in array_to_string(textcolumns,','))+4),(POSITION('//' in array_to_string(textcolumns,','))-POSITION('NMSC' in array_to_string(textcolumns,','))-4)))
-               END
+         -- CITI UK
+         WHEN ecosysaccount ilike ('CLIENT_FUNDS_UNITED_KINGDOM_CITI%') THEN 
+            CASE 
+               WHEN ( POSITION('NMSC' IN array_to_string(textcolumns, ',')) ) = 0 THEN ( SUBSTRING( array_to_string(textcolumns, ','), ( POSITION('NTRF' IN array_to_string(textcolumns, ',')) + 4 ), 10 ) )
+               ELSE ( SUBSTRING( array_to_string(textcolumns, ','), ( POSITION('NMSC' IN array_to_string(textcolumns, ',')) + 4 ), ( POSITION('//' IN array_to_string(textcolumns, ',')) - POSITION('NMSC' IN array_to_string(textcolumns, ',')) -4 ) ) )
+            END
+         -- SEB GERMANY
+         WHEN ecosysAccount ILIKE ('CLIENT_FUNDS_GERMANY_ESSE%') THEN SUBSTRING( array_to_string(textcolumns, ','), POSITION('FRA' IN array_to_string(textcolumns, ',')) + 4, 11 )
          ELSE NULL
-      END AS "bankreferencenumber"
+      END AS "bank_reference_number"
    FROM
       mt94xparser.View_All_Rows
    WHERE
-      reference = :'bankwithdrawalid'
-      AND amount < 0
-),
-cte AS (
+      BankLedgerID = ( SELECT bankledgerid FROM payout_info )
+)
+SELECT
+   'CSV' AS "statement",
+   ledgerrowid,
+   ecosysaccount,
+   currency,
+   amount,
+   balancedate,
+   datecolumn1 AS "valuedate",
+   textcolumns,
+   bankledgerid
+FROM
+   ledger.view_all_rows
+WHERE
+   ecosysAccount = ( SELECT "bank" FROM payout_info)
+   AND balanceDate BETWEEN ((SELECT "date" FROM payout_info)) AND ((SELECT "date" FROM payout_info) + (SELECT "search_period" FROM payout_info))
+   AND currency = (SELECT "currency" FROM payout_info)
+   AND amount BETWEEN (SELECT "min_amount" FROM payout_info) AND (SELECT "max_amount" FROM payout_info)
+   AND (
+         (array_to_string(textcolumns,',') ilike '%' || (SELECT "accountnumber" FROM payout_info) || '%')
+      OR (array_to_string(textcolumns,',') ilike '%' || (SELECT "name" FROM payout_info) || '%')
+      OR (array_to_string(textcolumns,',') ilike '%' || (SELECT "reference" FROM payout_info) || '%')
+      OR (array_to_string(textcolumns,',') ilike '%' || (SELECT "bank_reference_number" FROM bank_reference_number) || '%')
+      )
+
+UNION
+
+SELECT
+   'MT940' AS "statement",
+   statementlineid,
+   ecosysaccount,
+   currency,
+   amount,
+   balancedate,
+   valuedate,
+   textcolumns,
+   bankledgerid
+FROM
+   mt94xparser.View_All_Rows
+WHERE
+   ecosysAccount = ( SELECT "bank" FROM payout_info)
+   AND valuedate BETWEEN ((SELECT "date" FROM payout_info)) AND ((SELECT "date" FROM payout_info) + (SELECT "search_period" FROM payout_info))
+   AND currency = (SELECT "currency" FROM payout_info)
+   AND amount BETWEEN (SELECT "min_amount" FROM payout_info) AND (SELECT "max_amount" FROM payout_info)
+   AND (
+         (array_to_string(textcolumns,',') ilike '%' || (SELECT "accountnumber" FROM payout_info) || '%')
+      OR (array_to_string(textcolumns,',') ilike '%' || (SELECT "name" FROM payout_info) || '%')
+      OR (array_to_string(textcolumns,',') ilike '%' || (SELECT "reference" FROM payout_info) || '%')
+      OR (array_to_string(textcolumns,',') ilike '%' || (SELECT "bank_reference_number" FROM bank_reference_number) || '%')
+      );
+
+\echo '*******************'
+\echo '*   BANK LEDGER   *'
+\echo '*******************\n'
+
+WITH payout_info AS(
    SELECT
-      w.bankwithdrawalid as "reference",
+      w.bankledgerid,
+      w.bankwithdrawalid AS "reference",
       w.sendingbankaccountid AS "bankaccountid",
-      w.datestamp::DATE AS "date",
+      w.datestamp :: DATE AS "date",
       w.currency AS "currency",
-      w.amount AS "amount",
       CASE
          WHEN (w.amount - (100 * cr.ask)) < 0 THEN 0
          ELSE (w.amount - (100 * cr.ask))
-      END AS "amountminusfee",
+      END AS "min_amount",
+      w.amount AS "max_amount",
       w.name AS "name",
-      w.toaccountnumber AS "accountnumber"
+      w.toaccountnumber AS "accountnumber",
+      ba.ecosysaccount AS "bank",
+      CASE
+         WHEN (tba.clearinghouseid = 44) THEN '1 month' :: INTERVAL
+         ELSE '14 days' :: INTERVAL
+      END AS "search_period"
    FROM
       bankwithdrawals w
-      LEFT JOIN currencypairs cp ON (w.currency = cp.quotecurrency AND cp.basecurrency = 'EUR')
+      LEFT JOIN bankaccounts ba ON ba.bankaccountid = w.sendingbankaccountid
+      LEFT JOIN transferbankaccounts tba ON tba.transferbankaccountid = w.transferbankaccountid
+      LEFT JOIN currencypairs cp ON ( w.currency = cp.quotecurrency AND cp.basecurrency = 'EUR')
       LEFT JOIN currencyexchangerates cr ON (cp.currencypairid = cr.currencypairid)
    WHERE
       bankwithdrawalid = :'bankwithdrawalid'
+),
+bank_reference_number AS (
+   -- Add new case for each bank which generate new bankreferencenumber (CSV statement)
+   SELECT
+      CASE
+         -- LHV ESTONIA
+         WHEN ecosysaccount ilike 'CLIENT_FUNDS_ESTONIA_LHVB%' THEN ( SUBSTRING( array_to_string(textcolumns, ','), POSITION( 'AcctSvcrRef:' IN array_to_string(textcolumns, ',') ) + 15, 30 ) )
+         ELSE NULL
+      END AS "bank_reference_number"
+   FROM
+      ledger.view_all_rows
+   WHERE
+      bankledgerid = ( SELECT bankledgerid FROM payout_info )
+
+   UNION
+
+   -- Add new case for each bank which generate new bankreferencenumber (MT940  statement)
+   SELECT
+      CASE
+         -- CITI UK
+         WHEN ecosysaccount ilike ('CLIENT_FUNDS_UNITED_KINGDOM_CITI%') THEN 
+            CASE 
+               WHEN ( POSITION('NMSC' IN array_to_string(textcolumns, ',')) ) = 0 THEN ( SUBSTRING( array_to_string(textcolumns, ','), ( POSITION('NTRF' IN array_to_string(textcolumns, ',')) + 4 ), 10 ) )
+               ELSE ( SUBSTRING( array_to_string(textcolumns, ','), ( POSITION('NMSC' IN array_to_string(textcolumns, ',')) + 4 ), ( POSITION('//' IN array_to_string(textcolumns, ',')) - POSITION('NMSC' IN array_to_string(textcolumns, ',')) -4 ) ) )
+            END
+         -- SEB GERMANY
+         WHEN ecosysAccount ILIKE ('CLIENT_FUNDS_GERMANY_ESSE%') THEN SUBSTRING( array_to_string(textcolumns, ','), POSITION('FRA' IN array_to_string(textcolumns, ',')) + 4, 11 )
+         ELSE NULL
+      END AS "bank_reference_number"
+   FROM
+      mt94xparser.View_All_Rows
+   WHERE
+      BankLedgerID = ( SELECT bankledgerid FROM payout_info )
 )
 SELECT
    *
 FROM
    view_bank_ledger
 WHERE
-   bankaccountid = (SELECT "bankaccountid" FROM cte)
-   AND date BETWEEN (SELECT "date" FROM cte) AND (SELECT "date" FROM cte) + :'daterange'::INTERVAL
-   AND currency = (SELECT "currency" FROM cte)
-   AND amount BETWEEN (SELECT "amountminusfee" FROM cte) AND (SELECT "amount" FROM cte)
+   bankaccountid = (SELECT "bankaccountid" FROM payout_info) 
+   AND date BETWEEN (SELECT "date" FROM payout_info) AND (SELECT "date" FROM payout_info) + (SELECT "search_period" FROM payout_info)
+   AND currency = (SELECT "currency" FROM payout_info)
+   AND amount BETWEEN (SELECT "min_amount" FROM payout_info) AND (SELECT "max_amount" FROM payout_info)
    AND (
-         (text ilike '%' || (SELECT "accountnumber" FROM cte) || '%')
-      OR (text ilike '%' || (SELECT "name" FROM cte) || '%')
-      OR (text ilike '%' || (SELECT "reference" FROM cte) || '%')
-      OR (text ilike '%' || (SELECT "bankreferencenumber" FROM tmp) || '%') );
+         (text ilike '%' || (SELECT "accountnumber" FROM payout_info) || '%')
+      OR (text ilike '%' || (SELECT "name" FROM payout_info) || '%')
+      OR (text ilike '%' || (SELECT "reference" FROM payout_info) || '%')
+      OR (text ilike '%' || (SELECT "bank_reference_number" FROM bank_reference_number) || '%') );
+
 \echo '*******************'
 \echo '*      OTHER      *'
 \echo '*******************\n'
-WITH cte AS (
+
+WITH payout_info AS (
    SELECT
-      w.bankwithdrawalid as "reference",
+      w.bankledgerid,
+      w.bankwithdrawalid AS "reference",
       w.sendingbankaccountid AS "bankaccountid",
-      w.datestamp::DATE AS "date",
+      w.datestamp :: DATE AS "date",
       w.currency AS "currency",
-      w.amount AS "amount",
       CASE
          WHEN (w.amount - (100 * cr.ask)) < 0 THEN 0
          ELSE (w.amount - (100 * cr.ask))
-      END AS "amountminusfee",
+      END AS "min_amount",
+      w.amount AS "max_amount",
       w.name AS "name",
-      w.toaccountnumber AS "accountnumber"
+      w.toaccountnumber AS "accountnumber",
+      ba.ecosysaccount AS "bank",
+      CASE
+         WHEN (tba.clearinghouseid = 44) THEN '1 month' :: INTERVAL
+         ELSE '14 days' :: INTERVAL
+      END AS "search_period"
    FROM
       bankwithdrawals w
-      LEFT JOIN currencypairs cp ON (w.currency = cp.quotecurrency AND cp.basecurrency = 'EUR')
+      LEFT JOIN bankaccounts ba ON ba.bankaccountid = w.sendingbankaccountid
+      LEFT JOIN transferbankaccounts tba ON tba.transferbankaccountid = w.transferbankaccountid
+      LEFT JOIN currencypairs cp ON ( w.currency = cp.quotecurrency AND cp.basecurrency = 'EUR')
       LEFT JOIN currencyexchangerates cr ON (cp.currencypairid = cr.currencypairid)
    WHERE
       bankwithdrawalid = :'bankwithdrawalid'
 )
 SELECT
    count(*) AS "POSSIBLE RETURNS",
-   'SELECT * FROM view_bank_ledger WHERE bankaccountid = ' || (SELECT "bankaccountid" FROM cte) || ' AND currency = ' || '''' || (SELECT "currency" FROM cte) || '''' || ' AND date BETWEEN ' || '''' || ((SELECT "date" FROM cte)) || '''' || ' AND ' || '''' || ((SELECT "date" FROM cte) + :'daterange'::INTERVAL) || '''' || ' AND amount = ' || (SELECT "amount" FROM cte) || ' AND claimable;' AS "QUERY TO RUN"
+   'SELECT * FROM view_bank_ledger WHERE bankaccountid = ' || (SELECT "bankaccountid" FROM payout_info) || ' AND currency = ' || '''' || (SELECT "currency" FROM payout_info) || '''' || ' AND date BETWEEN ' || '''' || ((SELECT "date" FROM payout_info)) || '''' || ' AND ' || '''' || ((SELECT "date" FROM payout_info) + (SELECT "search_period" FROM payout_info)) || '''' || ' AND amount = ' || (SELECT "max_amount" FROM payout_info) || ' AND claimable;' AS "QUERY TO RUN"
 FROM
    view_bank_ledger
 WHERE
-   bankaccountid = (SELECT "bankaccountid" FROM cte)
-   AND date BETWEEN (SELECT "date" FROM cte) AND ((SELECT "date" FROM cte) + :'daterange'::INTERVAL)
-   AND currency = (SELECT "currency" FROM cte)
-   AND amount = (SELECT "amount" FROM cte)
+   bankaccountid = (SELECT "bankaccountid" FROM payout_info)
+   AND date BETWEEN (SELECT "date" FROM payout_info) AND ((SELECT "date" FROM payout_info) + (SELECT "search_period" FROM payout_info))
+   AND currency = (SELECT "currency" FROM payout_info)
+   AND amount = (SELECT "max_amount" FROM payout_info)
    AND claimable;
-   
+
 \echo 'ALWAYS DOUBLE CHECK THE RESULT!\n'
+
+\pset expanded AUTO
+\timing ON
