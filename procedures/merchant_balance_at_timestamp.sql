396750a950c9eb56100b8e4e3dccabc183968420
Benjaminschembri <benjamin.schembri@trustly.com>
2022-01-05 18:27:19 +0100

    add all currencies to balance at timestamp request & total eur balance

--- procedures/merchant_balance_at_timestamp.sql
+++ procedures/merchant_balance_at_timestamp.sql
@@ -1,7 +1,7 @@
 /* Merchant's Balance at a certain date and time */
 
 \prompt 'please enter the processing account', processingaccount
-\prompt 'please enter a currency', currency
+--\prompt 'please enter a currency for the total converted amount', currency
 \prompt 'please enter datestamp with timestamp', dateandtime
 
 \set QUIET ON
@@ -16,12 +16,12 @@ WITH ClosingBalance AS (
    INNER JOIN FlagValues ON (FlagValueAccountingBalancesDaily.FlagValueID = FlagValues.FlagValueID)
    INNER JOIN Users ON (Users.Username = FlagValues.Value)
    WHERE AccountName = 'CLIENT_BALANCES'
-     AND Users.Username = :'processingaccount'
-     AND Date = (:'dateandtime')::date - interval '1 day'
-     AND Currency = :'currency'
+     AND Users.Username = :'processingaccount' --- to be declared as PA
+     AND Date = (:'dateandtime')::date - interval '1 day' --- to be declared as timestamp
+     AND Currency IN (SELECT currency FROM balance(:'processingaccount')) --- to be declared as PA
    ORDER BY Currency,Date
  ),
- AccountLedger AS (
+ AccountLedger AS(
    SELECT Users.Username,
           SUM(round(COALESCE(FXTrades.NewAmount * -1, CASE WHEN FlagValueAccountingTransactions.DebitAccountName = 'CLIENT_BALANCES' THEN -FlagValueAccountingTransactions.Amount ELSE FlagValueAccountingTransactions.Amount END),2)) AS TotalAmount,
           COALESCE(FXTrades.NewCurrency, FlagValueAccountingTransactions.Currency) AS Currency
@@ -34,17 +34,25 @@ WITH ClosingBalance AS (
      LEFT JOIN FXTrades ON (FXTrades.TransactionID = FlagValueAccountingTransactions.TransactionID)
     WHERE FlagValues.FlagID = CONST_Username_FlagID()
       AND Users.UserID = Get_UserID(:'processingaccount')
-      AND (COALESCE(FXTrades.NewCurrency,FlagValueAccountingTransactions.Currency) = :'currency')
+      AND (COALESCE(FXTrades.NewCurrency,FlagValueAccountingTransactions.Currency) IN (SELECT currency FROM balance(:'processingaccount'))) --- to be declared as PA = :'currency')
       AND FlagValueAccountingTransactions.RecordDate >= :'dateandtime'::date
       AND FlagValueAccountingTransactions.RecordDate <= :'dateandtime'
       AND 'CLIENT_BALANCES' IN (FlagValueAccountingTransactions.DebitAccountName, FlagValueAccountingTransactions.CreditAccountName)
       AND NOT EXISTS (SELECT 1 FROM FXTrades WHERE FXTrades.FXEventID = FlagValueAccountingTransactions.EventID AND FXTrades.EventID IS NOT NULL)
     GROUP BY 1,3
   )
-  SELECT ClosingBalance.Username,
-         ClosingBalance.Currency,
-         :'dateandtime' AS dateparameter,
-         ClosingBalance.Balance + COALESCE(AccountLedger.TotalAmount,0.00) AS Balance
-    FROM AccountLedger
-    RIGHT JOIN ClosingBalance ON ClosingBalance.Username = AccountLedger.Username AND ClosingBalance.Currency = AccountLedger.Currency
+  SELECT DISTINCT Currencies.processingaccount AS Processing_account,
+         Currencies.dateparameter AS date_parameter,
+         hstore(array_agg(DISTINCT array[Currencies.Currency::text, Currencies.Balance::text])) AS Currency_Balances,
+         totalbalanceeuro AS "total_balance_EUR"
+    FROM (
+      SELECT ClosingBalance.Username AS processingaccount,
+             :'dateandtime'::timestamp AS dateparameter,
+             ClosingBalance.Currency::text AS currency,
+             (ClosingBalance.Balance + COALESCE(AccountLedger.TotalAmount,0.00)) AS Balance,
+             ROUND(sum(fx_readonly(ClosingBalance.Currency, 'EUR', :'dateandtime'::date) * (ClosingBalance.Balance + COALESCE(AccountLedger.TotalAmount,0.00))) over(),2)  AS totalbalanceeuro
+        FROM AccountLedger
+       RIGHT JOIN ClosingBalance ON ClosingBalance.Username = AccountLedger.Username AND ClosingBalance.Currency = AccountLedger.Currency
+         ) Currencies
+   GROUP BY 1,2,4
 ;
