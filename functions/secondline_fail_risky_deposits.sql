31862c19658025f402121ad21a342fd345c4dd46
Benjaminschembri <benjamin.schembri@trustly.com>
2022-04-07 14:26:21 +0200

    improve session user check (2nd line only)

--- functions/secondline_fail_risky_deposits.sql
+++ functions/secondline_fail_risky_deposits.sql
@@ -2,8 +2,8 @@
 --Related to procedure: risky_deposits.sql
 
 CREATE OR REPLACE FUNCTION pg_temp.secondline_fail_risky_deposits(_tofail text)
-   RETURNS boolean
-   LANGUAGE plpgsql
+RETURNS boolean
+LANGUAGE plpgsql
 
 AS $function$
 
@@ -14,36 +14,40 @@ _count bigint;
 
 BEGIN
 
-SELECT user INTO _loggedinuser;
+  IF NOT EXISTS(
+    SELECT 1
+      FROM pg_auth_members
+      JOIN pg_roles member ON member.oid = pg_auth_members.member
+      JOIN pg_roles role ON role.oid=pg_auth_members.roleid
+     WHERE role.rolname = 'support_second_line'
+       AND member.rolname = session_user
+     ) THEN
+         RAISE EXCEPTION 'No Function access. 2ndline access only';
+  END IF;
 
-IF _loggedinuser IN ('clintonmicallef', 'tomasvebr', 'benjaminschembri', 'dimitriossliakas')
-  THEN
-    IF _tofail = 'yes'
-      THEN
-        RAISE NOTICE 'Failing risky depsits...';
+  IF _tofail = 'yes'
+    THEN
+      RAISE NOTICE 'Failing risky depsits...';
 
-        SELECT TransferID, SUM(COUNT(*)) over()
-          INTO _transferID,
-               _count
-          FROM pg_temp.secondline_view_risky_deposits()
-         GROUP BY 1;
+      SELECT TransferID, SUM(COUNT(*)) over()
+        INTO _transferID,
+             _count
+        FROM pg_temp.secondline_view_risky_deposits()
+       GROUP BY 1;
 
-          IF _transferID IS NOT NULL
-            THEN PERFORM fail_deposit_transfer(TransferID) FROM pg_temp.secondline_view_risky_deposits();
-            RAISE NOTICE 'Failed % deposits', _count;
-            RETURN TRUE;
-          ELSE RAISE EXCEPTION 'No transfers to fail!';
-          END IF;
+        IF _transferID IS NOT NULL
+          THEN PERFORM fail_deposit_transfer(TransferID) FROM pg_temp.secondline_view_risky_deposits();
+          RAISE NOTICE 'Failed % deposits', _count;
+          RETURN TRUE;
+        ELSE RAISE EXCEPTION 'No transfers to fail!';
+        END IF;
 
-    ELSIF _tofail = 'no'
-      THEN RAISE NOTICE 'No actions taken. Exiting...';
+  ELSIF _tofail = 'no'
+    THEN RAISE NOTICE 'No actions taken. Exiting...';
 
-    ELSE RAISE EXCEPTION 'Incorrect input. Enter ''yes'' to fail deposit transfers or ''no'' to exit';
-    END IF;
+  ELSE RAISE EXCEPTION 'Incorrect input. Enter ''yes'' to fail deposit transfers or ''no'' to exit';
+  END IF;
 
-ELSE RAISE EXCEPTION 'Unauthorised Access - 2nd line access only';
-END IF;
-
-RETURN FALSE;
+RETURN true;
 END;
 $function$;
